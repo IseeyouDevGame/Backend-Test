fefdeb3bd6d7e1fc73add13200e6c1d1a8419d45
IseeyouDevGame <iseeyoudevstuff@gmail.com>
2024-12-15 23:26:27 -0500

    commit five

--- Calculator.ts
+++ Calculator.ts
@@ -44,3 +44,21 @@ function calculateReceipt(input) {
 
     return r;
 }
+
+const input1 = [
+    '2 book at 12.49',
+    '1 music CD at 14.99',
+    '1 chocolate bar at 0.85'
+];
+
+const input2 = [
+    '1 imported box of chocolates at 10.00',
+    '1 imported bottle of perfume at 47.50'
+];
+
+const input3 = [
+    '1 imported bottle of perfume at 27.99',
+    '1 bottle of perfume at 18.99',
+    '1 packet of headache pills at 9.75',
+    '3 imported boxes of chocolates at 11.25'
+];
